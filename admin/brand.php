44f27e6396f31ad145dd5f4b7a32aa673d1eb132
Huzaifa109837 <hamim109837@gmail.com>
2023-01-27 02:39:00 +0600

    brand page added

---
+++ admin/brand.php
@@ -0,0 +1,23 @@
+<?php include'include/header.php';?>
+
+<?php include'include/sidebar.php';?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Dashboard</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+        <!-- your main code here -->
+    </section>
+
+  </main><!-- End #main -->
+
+<?php include'include/footer.php';?>
--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -44,7 +44,7 @@
             </a>
           </li>
           <li>
-            <a href="tables-data.html">
+            <a href="brand.php">
               <i class="bi bi-circle"></i><span>Brand</span>
             </a>
           </li>
